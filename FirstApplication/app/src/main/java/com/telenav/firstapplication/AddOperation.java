760a43bbb69e879d3d10828ebbdb1302179a7768
raduuh <halmaciu_radu_93@yahoo.com>
2017-03-10 15:13:41 +0200

    Implemented add operation

---
+++ FirstApplication/app/src/main/java/com/telenav/firstapplication/AddOperation.java
@@ -0,0 +1,12 @@
+package com.telenav.firstapplication;
+
+/**
+ * Created by raduh on 3/10/2017.
+ */
+
+public class AddOperation {
+
+    public static int addOper(int a, int b) {
+        return a + b;
+    }
+}
