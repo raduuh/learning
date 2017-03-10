45ae917dc4944eb2fc7e328995eb41fcb8bd5309
raduuh <halmaciu_radu_93@yahoo.com>
2017-03-10 17:14:24 +0200

    Another commit

--- FirstApplication/app/src/main/java/com/telenav/firstapplication/AddOperation.java
+++ FirstApplication/app/src/main/java/com/telenav/firstapplication/AddOperation.java
@@ -19,5 +19,6 @@ public class AddOperation {
     }
     public static void newMethod(){
         // do something
+        //dsadsadsa
     }
 }
--- FirstApplication/app/src/main/java/com/telenav/firstapplication/TestRebase.java
+++
@@ -1,8 +0,0 @@
-package com.telenav.firstapplication;
-
-/**
- * Created by raduh on 3/10/2017.
- */
-
-public class TestRebase {
-}
